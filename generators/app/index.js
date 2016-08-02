9b8f6ddfbfa06c799227bce15149a092fa698209
Pouya Kary <k@karyfoundation.org>
2016-08-03 03:50:30 +0430

    now I have to complete the themes part and done...

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,36 +1,164 @@
-'use strict';
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
 
-module.exports = yeoman.Base.extend({
-  prompting: function () {
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the badass ' + chalk.red('generator-themex') + ' generator!'
-    ));
+//
+// Yeoman generator for themeX
+//   Copyright 2016 Kary Foundation, Inc. All rights reserved.
+//   Author: Pouya Kary <k@karyfoundation.org>
+//
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+//
+// ─── STRICT ─────────────────────────────────────────────────────────────────────
+//
 
-    return this.prompt(prompts).then(function (props) {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    }.bind(this));
-  },
+    'use strict';
 
-  writing: function () {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
-  },
+//
+// ─── IMPORTS ────────────────────────────────────────────────────────────────────
+//
 
-  install: function () {
-    this.installDependencies();
-  }
-});
+    var yeoman  = require('yeoman-generator');
+    var colors  = require('colors');
+    var yosay   = require('yosay');
+    var path    = require('path');
+
+//
+// ─── DEFS ───────────────────────────────────────────────────────────────────────
+//
+
+    let prompts = [
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?',
+            default: 'My beloved theme'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: "How do you describe your theme?",
+            default: 'Most beautiful scheme...'
+        },
+        {
+            type: 'input',
+            name: 'author',
+            message: "Who is the author?",
+            default: 'My name or my awesome organization name'
+        },
+        {
+            type: 'input',
+            name: 'filename',
+            message: "What's your base file name?",
+            default: 'some-name',
+            validate: fileNameValidator
+        },
+        {
+            type: 'list',
+            name: 'mode',
+            message: 'What starting theme modes you want?',
+            choices: [
+                'A dark theme',
+                'A light theme',
+                'Both light and dark themes'
+            ]
+        }
+    ];
+
+//
+// ─── MAIN ───────────────────────────────────────────────────────────────────────
+//
+
+    module.exports = yeoman.Base.extend({
+
+        //
+        // ─── PROMPTING ───────────────────────────────────────────────────
+        //
+
+            prompting: function ( ) {
+                welcomeYoSay( );
+                return this.prompt( prompts ).then( function ( props ) {
+                    this.props = props;
+                }.bind( this ));
+            },
+
+        //
+        // ─── WRITING ─────────────────────────────────────────────────────
+        //
+
+            writing: function () {
+                // Readme
+                this.fs.copyTpl(
+                    this.templatePath('README.md'),
+                    this.destinationPath('README.md'),
+                    { title: this.props.name }
+                );
+
+                this.fs.copyTpl(
+                    this.templatePath('package.json'),
+                    this.destinationPath('package.json'),
+                    { name: this.props.filename }
+                );
+
+                this.fs.copy(
+                    this.templatePath('travis.yml'),
+                    this.destinationPath('.travis.yml')
+                )
+
+                this.fs.copyTpl(
+                    this.templatePath('theme.yml'),
+                    this.destinationPath(
+                        path.join( this.props.filename,
+                            path.join( '.themeX', 'theme.yml' )
+                    )),
+                    generateThemeXFile( this.props )
+                );
+            },
+
+        // ─────────────────────────────────────────────────────────────────
+    });
+
+//
+// ─── WELCOME YO SAY ─────────────────────────────────────────────────────────────
+//
+
+    function welcomeYoSay ( ) {
+        console.log( yosay(
+            'Welcome to the\n' + 'themeX theme project\n'.rainbow + 'initializer!'
+        ));
+    }
+
+//
+// ─── GET BASE FILE NAME ─────────────────────────────────────────────────────────
+//
+
+    function nameFixer ( name ) {
+        return name.toLowerCase( ).replace( / |\/|\\/g , '-' );
+    }
+
+//
+// ─── BASE FILE NAME VALIDATOR ───────────────────────────────────────────────────
+//
+
+    function fileNameValidator ( basename ) {
+        return basename === nameFixer( basename )? true : nameFixer( basename );
+    }
+
+//
+// ─── GENERATE THEMEX FILE ───────────────────────────────────────────────────────
+//
+
+    function generateThemeXFile ( props ) {
+        return {
+            description: props.description,
+            author: props.author,
+            themes: generateThemes( props )
+        }
+    }
+
+//
+// ─── GENERATE THEMES ────────────────────────────────────────────────────────────
+//
+
+    function generateThemes ( props ) {
+        return props.mode.toString( );
+    }
+
+// ────────────────────────────────────────────────────────────────────────────────
